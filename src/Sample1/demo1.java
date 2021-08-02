85098a55316545443e2359e8897d57964ac60f1c
billu <billu@LAPTOP-09NLDVQ6>
2021-08-02 15:30:53 +0530

    [Shivakumar]:Project added to git

---
+++ src/Sample1/demo1.java
@@ -0,0 +1,33 @@
+package Sample1;
+
+public class demo1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("launch github");
+		System.out.println("launching github");
+		System.out.println("launching github");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
