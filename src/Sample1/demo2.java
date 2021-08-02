e2d9c26bfde1f7ce11c90d8878c899e4ee0e128b
billu <billu@LAPTOP-09NLDVQ6>
2021-08-02 15:53:42 +0530

    [Shiva]:committed demo2 class

---
+++ src/Sample1/demo2.java
@@ -0,0 +1,35 @@
+package Sample1;
+
+public class demo2 {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("gm");
+		
+		System.out.println("i dont want to say gm");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
